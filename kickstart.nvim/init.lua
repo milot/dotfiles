d1a32cb2a80f1375a37fb70e64ee23a9975d6423
Milot Shala <shala@milot.io>
2023-03-22 13:26:33 +0100

    Update init.lua

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -73,6 +73,18 @@ require('lazy').setup({
   {'nvim-tree/nvim-web-devicons', version = "*", config=true},
   {'sitiom/nvim-numbertoggle', version = "*"},
   {'hashicorp/terraform-ls', version = "*"},
+  {'kenn7/vim-arsync', version = "*"},
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
+
 
   -- Git related plugins
   'tpope/vim-fugitive',
